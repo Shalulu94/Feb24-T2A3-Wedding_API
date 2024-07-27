0adc8fab865ac0719f96ceb635022d34a94c59b0
shalulu94 <shahrulnasir290@hotmail.com>
2024-07-27 22:31:57 +1000

    fixed bugs or JWT token authentication

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,8 +1,13 @@
+from datetime import timedelta
+
 from flask import Blueprint, request
 from sqlalchemy.exc import IntegrityError
+from psycopg2 import errorcodes
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 from init import bcrypt, db
 from models.user import User, user_schema, UserSchema
+# from utils import auth_as_admin_decorator
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -34,8 +39,29 @@ def register_user():
         # respond back
         return user_schema.dump(user), 201
     
+    # catch exceptions
     except IntegrityError as err:
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
             return {"error": f"The column {err.orig.diag.column_name} is required"}, 409
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {"error": "Email address already in use"}, 409
+        
+
+@auth_bp.route("/login", methods=["POST"])
+def login_user():
+    # get the data from the body of the request
+    body_data = request.get_json()
+    # find the user in DB with that email address
+    stmt = db.select(User).filter_by(email=body_data.get("email"))
+    user = db.session.scalar(stmt)
+    # if user exists and password is correct
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        # create jwt
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        # respond back
+        return {"email": user.email, "is_admin": user.is_admin, "token": token}
+    
+    # else
+    else:
+        # respond back with an error message
+        return {"error": "Invalid email or password"}, 401
